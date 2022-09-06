260b50f7d006da5e742acc690c24de49e620b62b
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2022-09-06 15:38:12 -0300

    initial commit

---
+++ src/main/java/com/example/demo/Courseproject1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Courseproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Courseproject1Application.class, args);
+	}
+
+}
