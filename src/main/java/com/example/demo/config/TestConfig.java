f882998f11134c4d287bdd2202787745c9f7e7a7
JoaoVitorSD <joaovitorsantanadepollo@gmail.com>
2022-09-13 10:32:15 -0300

    addition of Product class

--- src/main/java/com/example/demo/config/TestConfig.java
+++ src/main/java/com/example/demo/config/TestConfig.java
@@ -11,9 +11,11 @@ import org.springframework.context.annotation.Profile;
 import com.example.demo.entities.Category;
 import com.example.demo.entities.Order;
 import com.example.demo.entities.OrderStatus;
+import com.example.demo.entities.Product;
 import com.example.demo.entities.User;
 import com.example.demo.repositories.CategoryRepository;
 import com.example.demo.repositories.OrderRepository;
+import com.example.demo.repositories.ProductRepository;
 import com.example.demo.repositories.UserRepository;
 
 @Configuration
@@ -25,6 +27,8 @@ public class TestConfig implements CommandLineRunner {
 	private OrderRepository orderRepository;
 	@Autowired
 	private CategoryRepository categoryRepository;
+	@Autowired
+	private ProductRepository productRepository;
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "João Depollo", "joao@gmail.com", "9888888", "12345");
@@ -35,10 +39,18 @@ public class TestConfig implements CommandLineRunner {
 		Category c1 = new Category(null,"Electronics");
 		Category c2 = new Category(null,"Books");
 		Category c3 = new Category(null,"Computers");
+		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum dolor sit amet, consectetur.", 90.5, "");
+		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
+		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
+		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(c1,c2,c3));
+		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+		
+		
 	}
 
 }
---
+++ src/main/java/com/example/demo/entities/Product.java
@@ -0,0 +1,101 @@
+package com.example.demo.entities;
+
+import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Transient;
+
+@Entity
+public class Product implements Serializable{
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private Long ID;
+	private String name;
+	private String dscription;
+	private Double price;
+	private String imgUrl;
+	
+	
+	@Transient
+	private Set<Category> categories = new HashSet<>();
+
+	public Product() {
+	}
+
+	public Product(Long iD, String name, String dscription, Double price, String imgUrl) {
+		super();
+		ID = iD;
+		this.name = name;
+		this.dscription = dscription;
+		this.price = price;
+		this.imgUrl = imgUrl;
+	}
+
+	public Set<Category> getCategories() {
+		return categories;
+	}
+
+	public Long getID() {
+		return ID;
+	}
+
+	public void setID(Long iD) {
+		ID = iD;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDscription() {
+		return dscription;
+	}
+
+	public void setDscription(String dscription) {
+		this.dscription = dscription;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImgUrl() {
+		return imgUrl;
+	}
+
+	public void setImgUrl(String imgUrl) {
+		this.imgUrl = imgUrl;
+	};
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(ID);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(ID, other.ID);
+	}
+}
